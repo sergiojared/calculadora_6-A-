4cbfdbdcb11bf9916b976ce3e0b2412678984039
sergiojared <31052297+sergiojared@users.noreply.github.com>
2017-08-22 19:18:11 -0500

    Add files via upload

---
+++ Calculadora_6A/Calculadora_6A.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora_6A.Libreria;
+
+namespace Calculadora_6A.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos misMetodos = new Metodos();
+            Console.WriteLine(misMetodos.Saludo());
+            Console.WriteLine(misMetodos.Sumar(10, 58));
+            Console.WriteLine(misMetodos.Restar(50, 24));
+            Console.WriteLine(misMetodos.Multiplicar(6, 5));
+            Console.WriteLine(misMetodos.Dividir(8, 3));
+            Console.ReadKey();
+        }
+    }
+}
